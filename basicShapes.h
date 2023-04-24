6e9adcbba1287c143452f171b4dfc55cd6bf515d
vlovo <MLeitz@boptics.de>
2023-04-24 19:18:33 +0200

    fix  regex for pad / line use namespace  in basicShapes.h

--- basicShapes.h
+++ basicShapes.h
@@ -1,83 +1,86 @@
 #pragma once
-struct BoundingBox
+namespace  pcb2svg
 {
-    double x1, y1, x2, y2;
-
-    BoundingBox()
+    struct BoundingBox
     {
-        x1 = 10000000;
-        x2 = -1000000;
-        y1 = 10000000;
-        y2 = -1000000;
+        double x1, y1, x2, y2;
 
-    }
-    void bounds(double x, double y)
-    {
-        x1 = std::min(x, x1);
-        x2 = std::max(x, x2);
+        BoundingBox()
+        {
+            x1 = 10000000;
+            x2 = -1000000;
+            y1 = 10000000;
+            y2 = -1000000;
 
-        y1 = std::min(y, y1);
-        y2 = std::max(y, y2);
+        }
+        void bounds(double x, double y)
+        {
+            x1 = std::min(x, x1);
+            x2 = std::max(x, x2);
+
+            y1 = std::min(y, y1);
+            y2 = std::max(y, y2);
+        };
     };
-};
 
-BoundingBox box;
+    BoundingBox box;
 
-struct Brush
-{
-    Brush(double w) : width(w)
+    struct Brush
     {
-    }
-    double width;
-};
+        Brush(double w) : width(w)
+        {
+        }
+        double width;
+    };
 
-struct Unknown
-{
-     
-};
-
-struct Circle
-{
-    Circle(double d) : d(d)
+    struct Unknown
     {
-    }
-    double d;
-};
 
-struct Rect
-{
-    Rect(double w, double h) : width(w), height(h)
+    };
+
+    struct Circle
     {
-    }
+        Circle(double d) : d(d)
+        {
+        }
+        double d;
+    };
 
-    double width, height;
-
-};
-
-struct RoundRect
-{
-    RoundRect(double w, double h, double r) : width(w), height(h), radius(r)
+    struct Rect
     {
-    }
+        Rect(double w, double h) : width(w), height(h)
+        {
+        }
 
-    double width, height, radius;
+        double width, height;
 
-};
+    };
 
-struct RoundDonut
-{
-    RoundDonut(double od, double id ) : od(od), id(id) 
+    struct RoundRect
     {
-    }
+        RoundRect(double w, double h, double r) : width(w), height(h), radius(r)
+        {
+        }
 
-    double od, id; 
+        double width, height, radius;
 
-};
+    };
 
-struct Oval
-{
-    Oval(double w, double h) : width(w), height(h)
+    struct RoundDonut
     {
-    }
-    double width, height;
-};
+        RoundDonut(double od, double id) : od(od), id(id)
+        {
+        }
+
+        double od, id;
+
+    };
+
+    struct Oval
+    {
+        Oval(double w, double h) : width(w), height(h)
+        {
+        }
+        double width, height;
+    };
+}
